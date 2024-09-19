26cfcb3a6ba34740a0b9280d0890e3a38237cc5e
Mercenary <oaishazher@gmail.com>
2024-09-19 23:49:55 +0530

    API URL Changed

--- src/components/docs/DocsBody.tsx
+++ src/components/docs/DocsBody.tsx
@@ -3,9 +3,13 @@ import s from "./DocsBody.module.css";
 import {HiOutlineViewGrid} from "react-icons/hi";
 import {FaChevronLeft} from "react-icons/fa6";
 import Link from "next/link";
-import {useState} from "react";
+import {IoEye} from "react-icons/io5";
+import {MdDelete, MdOutlineFileDownload} from "react-icons/md";
+import Image from "next/image";
 
 export default function DocsBody() {
+
+
     return (
         <div className={s.docsContainer}>
             <div className={s.header}>
@@ -14,29 +18,38 @@ export default function DocsBody() {
                 <div className={s.switch}><HiOutlineViewGrid/></div>
             </div>
             <div className={s.container}>
-                <DocItem/>
-                <DocItem/>
-                <DocItem/>
+                <DocItem coverImage="/images/Question.svg" title="SE-COMPS B Timetable" type="pdf"/>
+                <DocItem coverImage="/images/Question.svg" title="IA1 Solution" type="png"/>
+                <DocItem coverImage="/images/Question.svg" title="Question" type="pdf"/>
             </div>
         </div>
     );
 }
 
-const DocItem = () => {
-    const [isExpanded, setIsExpanded] = useState(false);
+type DocItemProps = {
+    title: string,
+    type: string,
+    fileId?: string,
+    coverImage: string
+}
 
+const DocItem = ({
+                     title, type, coverImage, fileId
+                 }: DocItemProps) => {
     return <>
-        <div className={`${s.docItemBase} ${isExpanded && s.expanded}`} onClick={() => setIsExpanded(!isExpanded)}>
+        <div className={s.docItemBase}>
             <div className={s.coverImageBase}>
-                <img src="/images/question.svg" alt="" className={s.coverImage}/>
+                <Image src={coverImage} width={40} height={40} alt="" className={s.coverImage}/>
             </div>
             <div className={s.docDetails}>
-                <div className={s.title}>Title</div>
+                <div className={s.docTitle}>{title}</div>
                 <div className={s.tags}>
-                    <div className={s.tag}>pdf</div>
+                    <div className={s.tag} style={{backgroundColor: `var(--${type}-tag-color)`}}>{type}</div>
+                    <button className={`${s.tag} ${s.viewTag}`}><IoEye/> view</button>
+                    <button className={`${s.tag} ${s.downloadTag}`}><MdOutlineFileDownload/> download</button>
+                    <button className={`${s.tag} ${s.deleteTag}`}><MdDelete/> delete</button>
                 </div>
             </div>
-            <div></div>
         </div>
     </>;
 }
